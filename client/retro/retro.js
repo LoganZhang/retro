89102969869e3af6ce9a62c76c885483f4eccda1
LoganZhang <sei_zh@hotmail.com>
2015-07-15 10:42:56 +0800

    first commit

---
+++ client/retro/retro.js
@@ -0,0 +1,5 @@
+var retroModule = angular.module("retrospective");
+retroModule.controller("RetrospectiveCtrl", ['$scope', '$meteor',
+  function($scope, $meteor){
+    $scope.sections = [{name:'good'},{name:'bad'},{name: 'keep'}];
+}]);
